5e1cfb8e711d7fad805ca6091c7513b868509da9
Khangwelo1 <khangwelokevinm@gmail.com>
2024-02-15 14:27:17 +0200

    Added IDs in the table to target with IDs and fix bugs

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -6,7 +6,7 @@
 
 
 //  const [data, setData] = useState([{
-//   title:'title1',
+//   id:'id1',
 //   description:'description',
 //   owner:'owner',
 //   status:'status',
@@ -16,7 +16,7 @@
 //   comment:'comment'
 //  },
 //  {
-//   title:'title2',
+//   id:'id2',
 //   description:'description',
 //   owner:'owner',
 //   status:'status',
@@ -30,7 +30,7 @@
 
 //    const addRow=()=>{
 //       const singlerow= {
-//             title:'',
+//             id:'',
 //             description:'',
 //             owner:'',
 //             status:'',
@@ -43,12 +43,12 @@
 
 //    }
 
-//    const handleDelete = (title) =>{
-//     const newfields  = data.filter((filter)=> filter.title !== title);
+//    const handleDelete = (id) =>{
+//     const newfields  = data.filter((filter)=> filter.id !== id);
 //     setData(newfields)
 //    }
-//    const accessSelectValue = (e,title)=>{
-//     const demo = data.find((find)=>find.title===title)
+//    const accessSelectValue = (e,id)=>{
+//     const demo = data.find((find)=>find.id===id)
 //     demo.status = e.target.value;
 //     setData(data)
 //    }
@@ -60,7 +60,7 @@
 //        <div className="table">
 //         <div className="field-row  page-row">
 //           <div className="row-option"><i className="lni lni-more-alt"></i></div>
-//           <div className='field-name column-field'>Title</div>
+//           <div className='field-name column-field'>id</div>
 //           <div className='field-name'>Description</div>
 //           <div className='field-name'>Owner</div>
 //           <div className='field-name'>Status</div>
@@ -70,16 +70,16 @@
 //           <div className='field-name'>Comment</div>
 //         </div>
 //         {data.map((data)=>
-//         <div className="field-row  page-row"key={data.title}>
-//           <div className="row-option" onClick={()=>handleDelete(data.title)}><i className="lni lni-more-alt"></i></div>
-//           <div className='field-name row-data'>{data.title}</div>
+//         <div className="field-row  page-row"key={data.id}>
+//           <div className="row-option" onClick={()=>handleDelete(data.id)}><i className="lni lni-more-alt"></i></div>
+//           <div className='field-name row-data'>{data.id}</div>
 //           <div className='field-name row-data'>{data.description}</div>
 //           <div className='field-name row-data'>{data.owner}</div>
 //           <div className='field-name row-data'>{data.startDate}</div>
 //           <div className='field-name row-data'>{data.endDate}</div>
 //           <div className='field-name row-data'>{data.priority}</div>
 //           <div className='field-name row-data'>
-//             <select name="" id="" onChange={(e)=>accessSelectValue(e, data.title)}>
+//             <select name="" id="" onChange={(e)=>accessSelectValue(e, data.id)}>
 //               <option value="done">done</option>
 //               <option value="todo">todo</option>
 //               <option value="move">done</option>
@@ -107,7 +107,8 @@ import "./styles/style.css";
 function Table() {
   const [data, setData] = useState([
     {
-      title: 'title1',
+      id: 1,
+      title: 'king1',
       description: 'description',
       owner: 'owner',
       status: 'status',
@@ -117,30 +118,33 @@ function Table() {
       comment: 'comment'
     },
     {
-      title: 'title2',
+      id: 2,
+      title: 'king2',
       description: 'description2',
       owner: 'owner2',
       status: 'status2',
       startDate: '12-12-122',
       endDate: '12-12-122',
-      priority: 'critical2',
+      priority: 'critical',
       comment: 'comment2'
     }
     ,
     {
-      title: 'title3',
+      id: 3,
+      title: 'king',
       description: 'description3',
       owner: 'owner3',
       status: 'status3',
       startDate: '12-12-122',
       endDate: '12-12-122',
-      priority: 'critical2',
+      priority: 'critical',
       comment: 'comment3'
     }
   ]);
-
-  const addRow = () => {
+//To Add a field
+  const addRow = (id, title) => {
     const singleRow = {
+      id: '',
       title: '',
       description: '',
       owner: '',
@@ -150,33 +154,40 @@ function Table() {
       priority: '',
       comment: ''
     };
-    setData(prevArray => [...prevArray, singleRow]);
+    
+      const rowID = data.find(row => row.id === id );
+      const lastId = data.length > 0 ? data[data.length - 1].id : 0; // Get the last ID in the array
+      const nextIDRows = { ...rowID, id: lastId + 1};
+    setData(prevArray => [...prevArray, nextIDRows]);
   };
   // The funtcioto check delete is equal to the drop down valu
-  const handleDropdownChange = (event, title) => {
+  const handleDropdownChange = (event, id, title) => {
     if (event.target.value === "delete") {
-      handleDelete(title);
+      handleDelete(id);
     } else if(event.target.value === "duplicate"){
-      duplication(title);
+      duplication(id, title);
     }
   };
+//To duplicate the selected field
+  const duplication = (id, title) => {
+    const rowDuplication = data.find(row => row.id === id && row.title === title);
+    const lastId = data.length > 0 ? data[data.length - 1].id : 0; // Get the last ID in the array
+    const changedRows = { ...rowDuplication, id: lastId + 1, title: title + "copy" };
 
-  const duplication = (title) => {
-    const rowDuplication = data.find(row => row.title === title);
-    const changedRows = { ...rowDuplication, title:  title + " copy"};
     setData(prevData => [...prevData, changedRows]);
+
   };
 
-
-  const handleDelete = (title) => {
-    const newFields = data.filter((filter) => filter.title !== title);
+//To delete the selected field
+  const handleDelete = (id) => {
+    const newFields = data.filter((filter) => filter.id !== id);
     setData(newFields);
   };
 
 
-  const accessSelectValue = (e, title) => {
+  const accessSelectValue = (e, id) => {
     const updatedData = data.map(row =>
-      row.title === title ? { ...row, status: e.target.value } : row
+      row.id === id ? { ...row, status: e.target.value } : row
     );
     setData(updatedData);
   };
@@ -186,21 +197,32 @@ function Table() {
       <div className="container">
         <div className="table">
           <div className="field-row  page-row">
-            <div className="row-option"></div>
-            <div className='field-name column-field'>Title</div>
+            <div className="row-option"> 
+            
+            {/* To Be deleted */}
+            <select className="Dropdown-menu" id="" onChange={(e) => handleDropdownChange(e, data.id, data.title)}>
+                  <option value="drop">Drop</option>
+                  <option value="delete">Delete</option>
+                  <option value="duplicate">Duplicate</option>
+                </select>
+                {/* Deleting ends here */}
+                </div>
+            <div className='field-name column-field'>title</div>
             <div className='field-name'>Description</div>
             <div className='field-name'>Owner</div>
-            {/* <div className='field-name'>Status</div> */}
             <div className='field-name'>Start-Date</div>
             <div className='field-name'>Due-Date</div>
             <div className='field-name'>Priority</div>
             <div className='field-name'>Status</div>
             <div className='field-name'>Comment</div>
+            <div className='field-name'>id</div>
+
+            
           </div>
           {data.map((data) =>
-            <div className="field-row  page-row" key={data.title}>
+            <div className="field-row  page-row" key={data.id}>
               <div className="row-option" >
-                <select className="Dropdown-menu" id="" onChange={(e) => handleDropdownChange(e, data.title)}>
+              <select className="Dropdown-menu" id="" onChange={(e) => handleDropdownChange(e, data.id, data.title)}>
                   <option value="drop">Drop</option>
                   <option value="delete">Delete</option>
                   <option value="duplicate">Duplicate</option>
@@ -214,17 +236,19 @@ function Table() {
               <div className='field-name row-data'>{data.endDate}</div>
               <div className='field-name row-data'>{data.priority}</div>
               <div className='field-name row-data'>
-                <select className="Dropdown-for-status" id="" onChange={(e) => accessSelectValue(e, data.title)}>
+                <select className="Dropdown-for-status" id="" onChange={(e) => accessSelectValue(e, data.id)}>
                   <option value="todo" style={{backgroundColor:'green'}}>todo</option>
                   <option value="doing">doing</option>
                   <option value="done">done</option>
                 </select>
               </div>
               <div className='field-name row-data'>{data.comment}</div>
+              <div className='field-name row-data'>{data.id}</div>
             </div>
           )}
         </div>
         <button onClick={addRow}>Add Row</button>
+        
       </div>
     </>
   );
